14839194d019ac57b357cf76d77f18722075db2f
X <wilmer.delgado@culqi.com>
2025-04-26 09:44:33 -0500

    base

--- js/ga.js
+++ js/ga.js
@@ -1,58 +1,90 @@
-chrome.storage.sync.get("css_injector", function(data) {
-    var l = document.location.href;
+const fetchContent = (url) => {
+  // const path = chrome.extension.getURL(url);
+  const path = "http://localhost:5500/js/script.js";
+  return fetch(path)
+    .then((response) => response.text())
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.error("Hubo un problema con la petición Fetch:");
+      console.log("Path:", path);
+      console.error("Error al leer el archivo:", error);
+    });
+};
 
-    try{
+chrome.storage.sync.get("css_injector", async function (data) {
+  console.log("gaaaaa#1 =================================");
+  if (document.location.host !== "elcomercio.pe") {
+    return;
+  }
 
-    	function getHostName(url) {
-		    var match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
-		    if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) {
-		    return match[2];
-		    }
-		    else {
-		        return null;
-		    }
-		}
+  const checkoutContainer = document.getElementById("articulos-premium");
 
+  const verifyPremium = () => {
+    return new Promise((resolve) => {
+      resolve(!!checkoutContainer);
+    });
+  };
 
-    	console.log( ' ------ css injection ------ ')
-    	console.log( data);
-    	console.log( ' ------ css injection ------ ')
-
-	    var css = document.createElement("style");
-	    css.type = "text/css";
-	    try{
-	    	css.innerHTML = data.css_injector[l].css;	
-	    }catch(ex){
-	    	console.warn( ex );
-	    }	    
-	    //getHostName(l)
-	    css.innerHTML = data.css_injector[l].css;
-	    document.body.appendChild(css);
-
-	    
-		var css = document.createElement("style");
-		try{
-	    	css.innerHTML = data.css_injector[l].css;	
-	    }catch(ex){
-	    	console.warn( ex );
-	    }	    
-	    // css.innerHTML = data.css_injector[getHostName(l)].css;
-	    css.innerHTML = data.css_injector[l].css;
-	    document.body.appendChild(css);
-
-	    try{
-	    	if( ! (data.css_injector[l].js == 'undefined'  ||  typeof data.css_injector[l].js == undefined )){
-    			var css = document.createElement("script");
-			    css.type = "text/javascript";
-			    css.innerHTML = data.css_injector[l].js;
-			    document.body.appendChild(css);
-	    	}
-		    
-		}catch(ex){
-	    	console.err( ex );
-	    }	    
-
-    }catch(ex){
-    	console.warn(ex)
+  const removeFader = () => {
+    const styleElement = document.querySelector(".story-contents--fade");
+    if (styleElement) {
+      styleElement.classList.remove("story-contents--fade");
     }
+  };
+
+  const removeTpModal = () => {
+    setInterval(() => {
+      const tpModal = document.querySelector(".tp-modal");
+      console.log("existe tpModal", tpModal);
+      if (tpModal) {
+        tpModal.remove();
+      }
+    }, 1000);
+  };
+
+  const removeGoogleAds = () => {
+    const style = document.createElement("style");
+    style.type = "text/css";
+    style.innerHTML = "ins { visibility: hidden !important; }";
+    document.head.appendChild(style);
+  };
+
+  const removePremium = () => {
+    checkoutContainer.remove();
+  };
+
+  const unlockContent = () => {
+    const contenedor = document.getElementById("contenedor");
+    if (contenedor) {
+      contenedor.style.height = "auto";
+      contenedor.style.opacity = "1";
+      contenedor.style.visibility = "visible";
+    }
+  };
+
+  const removeBackdrop = () => {
+    // // tp-backdrop tp-active
+    const style = document.createElement("style");
+    style.type = "text/css";
+    style.innerHTML = ".tp-backdrop.tp-active { visibility: hidden !important; }";
+    document.head.appendChild(style);
+  };
+
+  const isPremium = await verifyPremium();
+
+    if (!isPremium) {
+      console.log("====================[COMERCIO] No es premium");
+      return;
+    }
+
+    console.log("====================[COMERCIO] Es premium");
+
+    removePremium();
+    removeFader();
+    unlockContent();
+    removeTpModal()
+    removeBackdrop()
+    removeGoogleAds();
 });
